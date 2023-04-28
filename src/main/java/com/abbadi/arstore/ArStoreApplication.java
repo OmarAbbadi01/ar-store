509958c83632f4596fe99c9658f42bdd6bb9b1e0
Omar Abbadi <omar.abbadi2015@gmail.com>
2023-04-28 16:25:58 +0200

    establishing yaml files and database connection

---
+++ src/main/java/com/abbadi/arstore/ArStoreApplication.java
@@ -0,0 +1,13 @@
+package com.abbadi.arstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abbadi/arstore/ArStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abbadi.arstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
