873aed69214980281db01f56f12fcae6ce2c3558
Omar Abbadi <omar.abbadi2015@gmail.com>
2023-05-05 18:03:22 +0300

    adding cart to Customer

--- src/main/java/com/abbadi/arstore/customer/model/Customer.java
+++ src/main/java/com/abbadi/arstore/customer/model/Customer.java
@@ -2,6 +2,7 @@ package com.abbadi.arstore.customer.model;
 
 import com.abbadi.arstore.account.model.Account;
 import com.abbadi.arstore.address.model.Address;
+import com.abbadi.arstore.cart.model.Cart;
 import com.abbadi.arstore.common.Gender;
 import com.abbadi.arstore.common.generic.model.GenericEntity;
 import jakarta.persistence.*;
@@ -26,20 +27,24 @@ public class Customer implements GenericEntity<Long> {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "phone_number")
+    @Column(name = "phone_number", nullable = false)
     private String phoneNumber;
 
-    @Column(name = "gender")
+    @Column(name = "gender", nullable = false)
     private Gender gender;
 
     @OneToOne
-    @JoinColumn(name = "account_id")
+    @JoinColumn(name = "account_id", unique = true, nullable = false)
     private Account account;
 
     @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "customer_id")
+    @JoinColumn(name = "customer_id", nullable = false)
     private List<Address> addresses;
+
+    @OneToOne
+    @JoinColumn(name = "cart_id", unique = true, nullable = false)
+    private Cart cart;
 }
