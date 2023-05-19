55e318edd2078d641c685d8cda1112e5a88fe24f
Omar Abbadi <omar.abbadi2015@gmail.com>
2023-05-19 13:17:11 +0300

    adding / to allow https

--- src/main/java/com/abbadi/arstore/common/config/OpenApiConfig.java
+++ src/main/java/com/abbadi/arstore/common/config/OpenApiConfig.java
@@ -5,13 +5,16 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
         security = {
                 @SecurityRequirement(
                         name = "bearerAuth"
                 )
-        })
+        },
+        servers = @Server(url = "/")
+)
 @SecurityScheme(
         name = "bearerAuth",
         description = "JWT auth description",
