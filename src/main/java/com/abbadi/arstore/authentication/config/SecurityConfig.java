0594abe3b24cc01a8f6c3692989d243ecdb05efb
Omar Abbadi <omar.abbadi2015@gmail.com>
2023-05-19 12:16:51 +0300

    documenting api with openAPI via swagger

--- src/main/java/com/abbadi/arstore/authentication/config/SecurityConfig.java
+++ src/main/java/com/abbadi/arstore/authentication/config/SecurityConfig.java
@@ -30,7 +30,18 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**")
+                .requestMatchers("/api/auth/**",
+                        "/docs/**",
+                        "/v2/api-docs",
+                        "/v3/api-docs",
+                        "/v3/api-docs/**",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui/**",
+                        "/webjars/**",
+                        "/swagger-ui.html")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
